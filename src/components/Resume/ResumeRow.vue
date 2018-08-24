43f7db5ea4329cf8e9991a8503bd370dac43041d
ducpminh668 <ducpminh668@gmail.com>
2018-08-24 15:45:23 +0700

    add resume page

---
+++ src/components/Resume/ResumeRow.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex flex-row resume-row">
+    <div class="resume-row-label">
+      <span>
+        Professional Info
+      </span>
+    </div>
+    <div class="resume-row-description">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself.</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+$pinkColor: rgba(232, 53, 86, 1);
+$borderTopColor: rgba(139, 139, 139, 1);
+
+.resume-row {
+  border-top: 1px solid $borderTopColor;
+  padding: 30px 20px 50px 10px;
+  .resume-row-label {
+    width: 25%;
+    span {
+      width: 120px;
+      height: 120px;
+      display: block;
+      border-radius: 50%;
+      background-color: $pinkColor;
+      text-align: center;
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      color: #ffffff;
+    }
+  }
+  .resume-row-description {
+    width: 80%;
+    text-align: justify;
+    padding-top: 30px;
+  }
+}
+
+@media (max-width: 576px) {
+  .resume-row {
+    .resume-row-label {
+      span {
+        width: 76px;
+        height: 76px;
+        font-size: 11px;
+        padding: 0;
+        overflow: hidden;
+      }
+    }
+    .resume-row-description {
+      padding-left: 20px;
+      padding-top: 10px;
+    }
+  }
+}
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-8">
+        <ResumeRow />
+        <ResumeRow />
+      </div>
+      <div class="col-sm-2">
+        <div class="row">
+          <div class="col-6 col-sm-12 resume-skills mb-4">
+            <span class="label">
+              <i class="fa fa-exclamation" aria-hidden="true"></i>
+            </span>
+            <span>Skills</span>
+            <div class="hr"></div>
+            <div class="font-13">
+              <div class="text-uppercase">PHOTOSHOP FLASH
+              </div>
+              <p class="pt-2">Click to add a skill Click to add a skill Click to add a skill</p>
+            </div>
+          </div>
+          <div class="col-6 col-sm-12 resume-language">
+            <span class="label">
+              <i class="fa fa-globe" aria-hidden="true"></i>
+            </span>
+            <span>Languages</span>
+            <div class="hr"></div>
+            <div class="font-13">
+              <div class="text-uppercase">PHOTOSHOP FLASH
+              </div>
+              <p class="pt-2">Click to add a skill Click to add a skill Click to add a skill</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ResumeRow from '@/components/Resume/ResumeRow.vue'
+
+export default {
+  name: 'resume',
+  components: {
+    ResumeRow
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$cloudColor: rgba(97, 208, 212, 1);
+$borderColor: rgba(139, 139, 139, 1);
+
+.font-13 {
+  font-size: 13px;
+}
+.resume-skills,
+.resume-language {
+  .label {
+    width: 45px;
+    height: 45px;
+    display: block;
+    background-color: $cloudColor;
+    color: #ffffff;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 45px;
+    margin-bottom: 15px;
+  }
+  .hr {
+    border-top: 1px solid $borderColor;
+    margin: 10px 0px;
+  }
+}
+</style>
